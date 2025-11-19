c7ae3eda498f35d3f3af35d292f941adc8397f1f
kxianggen <xianggen13@gmail.com>
2025-11-19 14:49:58 +0800

    skills section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
 
@@ -15,6 +17,9 @@ export const Home = () => {
         <Navbar />
         {/* Main Content */}
         <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        
         {/* Footer */}
 
 
