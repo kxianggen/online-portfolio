fa69864c1b9df496f3c08011741442cde8a418ff
kxianggen <xianggen13@gmail.com>
2025-12-16 21:11:48 +0800

    resume update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/KongXiangGen_Resume_20251204.pdf"
+                href="/KongXiangGen_Resume_20251211.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
